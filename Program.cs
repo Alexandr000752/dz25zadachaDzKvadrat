83c667f9ce87caa26548cc4de387c4a93544f85a
Alexandr000752 <112328813+Alexandr000752@users.noreply.github.com>
2022-09-13 19:04:36 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Ведите число A");
+int A = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Ведите число B");
+int B = int.Parse(Console.ReadLine());
+
+int result = Convert.ToInt32(Math.Pow(A, B));
+
+Console.WriteLine(result);
